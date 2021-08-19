cf1dc180ba5b4d2cd6344608ae5512ba5c9d5b47
64rulez at gmail <64rulez@gmail.com>
2021-08-20 01:19:43 +0200

    Add files via upload

---
+++ PyMoIP/Server/arbo/EdtaTestMenu/Test2Complique.py
@@ -0,0 +1,7 @@
+# %arbo%/EdtaTestMenu/Test2Complique.py
+
+PageDir="/home/pi/python/PyMoIP/TestPages/Serveur-PyMoIP/TestEdta/"
+FirstFile=1
+LastFile=7
+PrefixFile="EDTA_MenuMT2"
+VarList=[[1,35,[],"%NumPageVdt",2,"/"],[1,37,[],"%NumPagesVdt",2," "]]
