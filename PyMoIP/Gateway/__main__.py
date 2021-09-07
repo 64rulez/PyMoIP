62edeba1c1c832b08d4340dfe057cdc801dac28a
64rulez at gmail <64rulez@gmail.com>
2021-09-07 19:31:12 +0200

    Add files via upload
    New feature 'DumpData'
    Few binary <=> unicode fix

--- PyMoIP/Gateway/__main__.py
+++ PyMoIP/Gateway/__main__.py
@@ -11,28 +11,17 @@ from Gateway.GatewayServer import GatewayServer
 # import asyncio to use its event loop
 import asyncio
 
-# Setup the logger
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
-                    level=logging.INFO,  # ERROR
-                    handlers=[
-                        logging.FileHandler("PyMoIP_gateway.log"),
-                        logging.StreamHandler(sys.stdout)
-                    ])
-
-# Redirect warnings to the logger
-logging.captureWarnings(True)
-warnings.simplefilter('always')
 
 
 parser = argparse.ArgumentParser(description="Launch the PyMoIP gateway.")
 parser.add_argument("--ws", type=int, metavar="PORT",
-                    help="Specify a port for a WebSocket Server. (Default=9001)")
+                    help="Specify a port for a WebSocket Server. (Default=9001)"
+                    "(If no port is provided, no WS Server will be created.)")                    
 parser.add_argument("--tcp", type=int, metavar="PORT",
                     help="Specify a port for a TCP Server. (Default=9000)"
                     "(If no port is provided, no TCP Server will be created.)")                    
 parser.add_argument("--display", type=int, metavar="PORT",
-                    help="Specify a port for a TCP display Server. (Default=8999)"
-                    "(If no port is provided, no TCP display Server will be created.)")                    
+                    help="Specify a port for a TCP display Server. (Default=8999)")
 parser.add_argument("-i", "--ip", type=str, metavar="IP",
                     help="Specify an IP address to listen to. (Default=0.0.0.0 (all @IPs))")
 parser.add_argument("-c", "--command", metavar="URI",
@@ -46,6 +35,13 @@ parser.add_argument("-b", "--subprotocols", metavar="PROTO",
                     help="Specify subprotocols for WS default videotext server (Default=[])")
 parser.add_argument("-n", "--nocommandchannel", metavar="SWITCH",
                     help="Disable command channel")
+parser.add_argument("-g", "--configpath", metavar="PATH",
+                    help="Path to configuration file (with a tailling '/')"
+                    "(must be RW, defaults to './Gateway/')")                    
+parser.add_argument("-l", "--logpath", metavar="PATH",
+                    help="Path to log file (with a tailling '/')"
+                    "(must be RW, defaults to '/home/pi/PyMoIP_dir/')")                    
+
 
 # TODO: add log level to the parser
 #zzparser.add_argument("--default-class", metavar="CLASS",
@@ -65,6 +61,8 @@ if __name__ == "__main__":
     teletel_server   = args.videotext
     teletel_interval = args.interval
     teletel_subproto = args.subprotocols
+    config_path      = args.configpath
+    log_path      = args.logpath
 
     if command_server is None :
       command_server = "ws://localhost:8764"
@@ -78,17 +76,36 @@ if __name__ == "__main__":
       command_server = None
     if MyIP is None:
       MyIP = "0.0.0.0"
-      
+    if config_path is None :
+      config_path = "./Gateway/"  # Must be RW as includes gateway launch count ... alternative might be in /home
+    if log_path is None :
+      log_path = "/home/pi/PyMoIP_dir/"  # "./"
+    if display_port is None:
+      display_port = 8999
+    
     # perform some error handling
     if ws_port is None and tcp_port is None:
         # default to WebSocket server on port 9001
         ws_port = 9001
-    elif ws_port == tcp_port:
-        print("Error: TCP server and WebSocket server cannot use the "
+    elif (ws_port == tcp_port) or (ws_port == display_port) or (display_port == tcp_port):
+        print("Error: TCP server, WebSocket server and Display server +cannot use the "
                 f"same port '{ws_port}'.\nProvide different ports "
                 "for each server.",
                 file=sys.stderr)
         exit(1)
+
+    # Setup the logger
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                        level=logging.INFO,  # ERROR
+                        handlers=[
+                            logging.FileHandler(log_path+"PyMoIP_gateway.log"),
+                            logging.StreamHandler(sys.stdout)
+                        ])
+    
+    # Redirect warnings to the logger
+    logging.captureWarnings(True)
+    warnings.simplefilter('always')
+
 
     if tcp_port is not None:
         if ws_port is None:
@@ -98,8 +115,6 @@ if __name__ == "__main__":
     else:
         logging.info("Launching a WebSocket Server on port '%d'", ws_port)
 
-    if display_port is None:
-      display_port = 8999
       
     if display_port is not None:
       logging.info("Launching a TCP display Server on port '%d'", display_port)
@@ -108,8 +123,8 @@ if __name__ == "__main__":
 
 
     try:
-        print("Target server '"+teletel_server+"' ping_interval="+str(teletel_interval)+" subprotocols="+teletel_subproto)
-        server = GatewayServer(MyIP, ws_port, tcp_port, display_port, command_server, teletel_server, teletel_interval , teletel_subproto)
+        print("Target server '"+teletel_server+"' ping_interval="+str(teletel_interval)+" subprotocols="+teletel_subproto+" configpath="+config_path)
+        server = GatewayServer(MyIP, ws_port, tcp_port, display_port, command_server, teletel_server, teletel_interval , teletel_subproto, config_path)
         
     # TODO: these excepts are no longer necessary, since port is bound until server.run() is called
     except PermissionError:
